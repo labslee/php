848d01ab6d3dce1fbcccfa9a9fe14d782eeb1ddf
labslee <59632223+labslee@users.noreply.github.com>
2020-01-08 11:16:36 +0900

    Create webhook.php

---
+++ webhook.php
@@ -0,0 +1,8 @@
+<?php
+echo "WEBHOOK TEST : \n" ;  
+$hookfile = $_SERVER['DOCUMENT_ROOT'] . "/test-".date('ymdhis').".json";
+$input = @file_get_contents("php://input");
+$event_json = json_encode(json_decode($input)); 
+file_put_contents($hookfile, $event_json);
+echo "- Done\n" ;  
+?>
